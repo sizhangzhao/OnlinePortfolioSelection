a0dc738c76901da3a60d84a56434cbd0220892b9
sizhangzhao <sz.zhao@foxmail.com>
2017-03-23 20:30:23 -0400

    Revised Expert File for Parallel

--- CORN/corn_expert.m
+++ CORN/corn_expert.m
@@ -52,16 +52,22 @@ Aeq = ones(1, N); beq = 1;
 lb = -0.1* ones(1, N); ub = ones(1, N);
 x0=ones(1, N)/N;
 select_data = past_data(C+1,:);
-regular = false;
-if ~regular
-    %without regularizer
-    obj= @(w) - (decay*log(select_data*w'));
-else
-    %with smoothing regularizer
-    obj= @(w) - (decay*log(select_data*w')) + 1*norm(ex_weight-w,2); 
+regular = true;
+weight = [];
+for para = 0.05:0.05:0.5
+    if ~regular
+        %without regularizer
+        obj= @(w) - (decay*log(select_data*w'));
+    else
+        %with smoothing regularizer
+        obj= @(w) - (decay*log(select_data*w')) + para*norm(ex_weight-w,2); 
+    end
+    options = optimset('LargeScale', 'off', 'Algorithm', 'active-set', 'Display', 'off');
+    weight1 = fmincon(obj, x0, A, b, Aeq, beq, lb, ub, [], options);
+    %disp(para)
+    %disp(norm(weight - ex_weight, 2))
+    weight1 = weight1';
+    weight = [weight, weight1];
 end
-options = optimset('LargeScale', 'off', 'Algorithm', 'active-set', 'Display', 'off');
-weight = fmincon(obj, x0, A, b, Aeq, beq, lb, ub, [], options);
-weight = weight';
 
 end
